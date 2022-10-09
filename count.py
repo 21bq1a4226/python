75c8f13024e66062b7b805df4d97468c39ade866
21bq1a4226 <113083133+21bq1a4226@users.noreply.github.com>
2022-10-09 21:12:10 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,4 @@
+list1 = [ ('Cat', 'Bat'), ('Sat', 'Cat'), ('Cat', 'Bat'),
+          ('Cat', 'Bat', 'Sat'), [1, 2], [1, 2, 3], [1, 2] ]
+print(list1.count(('Cat', 'Bat')))
+print(list1.count([1, 2]))
